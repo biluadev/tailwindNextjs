5cbfd38b6a7cfd53665532a546a39ba0c29efb60
biluadev <biluadevcod@gmail.com>
2024-01-09 20:20:51 +0100

    Começando a construir a Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-slate-900 p-8 text-slate-100">
-      <h1 className="flex items-center gap-3 text-5xl font-bold before:flex before:h-8 before:w-0.5 before:bg-sky-500">
-        Hello Tailwind
-      </h1>
-
-      <h2>Hello Tailwind</h2>
-      <p>Hello Tailwind</p>
-
-      <button className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium hover:bg-sky-600">
-        Sign in
-      </button>
-    </div>
-  )
+  return <h1>Hello Wold</h1>
 }
