d8636bc9675eea685458788d5f742c96cec8e4c5
biluadev <biluadevcod@gmail.com>
2024-01-21 17:49:15 +0100

    Feito a parte de upload

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -4,18 +4,18 @@ import { MainNavegation } from '../MainNavegation'
 import { NavItem } from '../MainNavegation/NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
-import { InputControl, InputPrefix, InputRoot } from '../Input'
+import * as Input from '../Input'
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <InputRoot>
-        <InputPrefix>
-          <Search />
-        </InputPrefix>
-        <InputControl placeholder="Search" />
-      </InputRoot>
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
 
       <MainNavegation />
 
